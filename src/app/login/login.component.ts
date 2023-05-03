ee831a61837b09501e9c52ac330e65354c976602
Parag More <parag.more@bikayi.com>
2023-05-03 18:34:29 +0530

    basic integration with login microservice

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,14 +1,35 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { DomSanitizer, SafeHtml } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss'],
 })
-export class LoginComponent  implements OnInit {
+export class LoginComponent implements OnInit {
+  loginHtml: SafeHtml | undefined;
+  constructor(private http: HttpClient, private sanitizer: DomSanitizer) {}
 
-  constructor() { }
+  ngOnInit() {
+    this.http
+      .get('http://localhost:8000/auth', { responseType: 'text' })
+      .subscribe((html: string) => {
+        this.loginHtml = this.sanitizer.bypassSecurityTrustHtml(html);
+        const div = document.createElement('div');
+        div.innerHTML = html;
+        const mainDiv = document.getElementById('login-ionic-container');
+        mainDiv?.appendChild(div)
+        const script = document.createElement('script');
+        if (div.querySelector('script') != null) {
+          console.log(div.querySelector('script') != null);
+          const scriptH = div.querySelector('script')?.innerHTML;
+          if (scriptH) {
+            script.innerHTML = scriptH;
+          }
+        }
 
-  ngOnInit() {}
-
+        document.body.appendChild(script);
+      });
+  }
 }
